6148bec790f788ea5bfef6b663dabfb0ee8f46d1
ganye <ganyekins@gmail.com>
2014-01-27 17:33:10 +0000

    accidentally left out the print statement

--- wifijammer.py
+++ wifijammer.py
@@ -122,7 +122,9 @@ def remove_mon_iface():
 def mon_mac(mon_iface):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     info = fcntl.ioctl(s.fileno(), 0x8927, struct.pack('256s', mon_iface[:15]))
-    return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
+    mac = ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
+    print '['+G+'*'+W+'] Monitor mode: '+G+mon_iface+W+' - '+O+mac+W
+    return mac
 
 ########################################
 # End of interface info and manipulation

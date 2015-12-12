33b191846f06e0e10e9a318cee872a2bc89558fa
Stefan Amyotte <stefan.amyotte@gmail.com>
2015-12-12 11:21:39 -0500

    add gitignore for virtualenv and breaklines for easier reading

--- wifijammer.py
+++ wifijammer.py
@@ -27,19 +27,62 @@ GR = '\033[37m' # gray
 T  = '\033[93m' # tan
 
 def parse_args():
-	#Create the arguments
+    #Create the arguments
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-s", "--skip", help="Skip deauthing this MAC address. Example: -s 00:11:BB:33:44:AA")
-    parser.add_argument("-i", "--interface", help="Choose monitor mode interface. By default script will find the most powerful interface and starts monitor mode on it. Example: -i mon5")
-    parser.add_argument("-c", "--channel", help="Listen on and deauth only clients on the specified channel. Example: -c 6")
-    parser.add_argument("-m", "--maximum", help="Choose the maximum number of clients to deauth. List of clients will be emptied and repopulated after hitting the limit. Example: -m 5")
-    parser.add_argument("-n", "--noupdate", help="Do not clear the deauth list when the maximum (-m) number of client/AP combos is reached. Must be used in conjunction with -m. Example: -m 10 -n", action='store_true')
-    parser.add_argument("-t", "--timeinterval", help="Choose the time interval between packets being sent. Default is as fast as possible. If you see scapy errors like 'no buffer space' try: -t .00001")
-    parser.add_argument("-p", "--packets", help="Choose the number of packets to send in each deauth burst. Default value is 1; 1 packet to the client and 1 packet to the AP. Send 2 deauth packets to the client and 2 deauth packets to the AP: -p 2")
-    parser.add_argument("-d", "--directedonly", help="Skip the deauthentication packets to the broadcast address of the access points and only send them to client/AP pairs", action='store_true')
-    parser.add_argument("-a", "--accesspoint", help="Enter the MAC address of a specific access point to target")
-    parser.add_argument("--world", help="N. American standard is 11 channels but the rest of the world it's 13 so this options enables the scanning of 13 channels", action="store_true")
+    parser.add_argument("-s",
+                        "--skip",
+                        help="Skip deauthing this MAC address. \
+                                Example: -s 00:11:BB:33:44:AA")
+    parser.add_argument("-i",
+                        "--interface",
+                        help="Choose monitor mode interface. \
+                                By default script will find the most powerful \
+                                interface and starts monitor mode on it. \
+                                Example: -i mon5")
+    parser.add_argument("-c",
+                        "--channel",
+                        help="Listen on and deauth only clients on the specified channel. \
+                                Example: -c 6")
+    parser.add_argument("-m",
+                        "--maximum",
+                        help="Choose the maximum number of clients to deauth. \
+                                List of clients will be emptied and repopulated \
+                                after hitting the limit. Example: -m 5")
+    parser.add_argument("-n",
+                        "--noupdate",
+                        help="Do not clear the deauth list when the maximum (-m) \
+                                number of client/AP combos is reached. \
+                                Must be used in conjunction with -m. \
+                                Example: -m 10 -n",
+                        action='store_true')
+    parser.add_argument("-t",
+                        "--timeinterval",
+                        help="Choose the time interval between packets being sent. \
+                                Default is as fast as possible. \
+                                If you see scapy errors like 'no buffer space' \
+                                try: -t .00001")
+    parser.add_argument("-p",
+                        "--packets",
+                        help="Choose the number of packets to send in each deauth burst. \
+                                Default value is 1; \
+                                1 packet to the client and 1 packet to the AP. \
+                                Send 2 deauth packets to the client \
+                                and 2 deauth packets to the AP: -p 2")
+    parser.add_argument("-d",
+                        "--directedonly",
+                        help="Skip the deauthentication packets to the broadcast \
+                                address of the access points and only send them \
+                                to client/AP pairs",
+                        action='store_true')
+    parser.add_argument("-a",
+                        "--accesspoint",
+                        help="Enter the MAC address of a specific access point to target")
+    parser.add_argument("--world",
+                        help="N. American standard is 11 channels but the rest \
+                                of the world it's 13 so this options enables the \
+                                scanning of 13 channels",
+                        action="store_true")
 
     return parser.parse_args()
 

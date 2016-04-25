a27cd48626b71f2828165621ff2e21d38172d1b2
Dan McInerney <dan.h.mcinerney@gmail.com>
2016-04-25 09:51:31 -0600

    Update wifijammer.py
    Fixed some upper case/lower case issues

--- wifijammer.py
+++ wifijammer.py
@@ -337,10 +337,11 @@ def cb(pkt):
 
             # Filter out all other APs and clients if asked
             if args.accesspoint:
-                if args.accesspoint not in [pkt.addr1, pkt.addr2]:
+                if args.accesspoint.lower() not in [pkt.addr1, pkt.addr2]:
                     return
 
-            if args.skip == pkt.addr2:
+            if args.skip:
+                if args.skip.lower() == pkt.addr2:
                 return
 
             # Check if it's added to our AP list

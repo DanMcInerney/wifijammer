1d0202f3560a71db6bc1d38069a28897aa58c5ac
Cassiano Vailati <cassiano.vailati@gmail.com>
2017-10-06 20:08:11 +0200

    Fixed args.skip array

--- wifijammer.py
+++ wifijammer.py
@@ -360,7 +360,7 @@ def cb(pkt):
 
             # Ignore all the noisy packets like spanning tree
 
-            if noise_filter(skip, pkt.addr1, pkt.addr2):
+            if noise_filter(args.skip, pkt.addr1, pkt.addr2):
                 return
 
             # Management = 1, data = 2
